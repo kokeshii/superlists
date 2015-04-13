3621b41cda506c6f9c59b1c9b368d4cfdf637f44
Carlos Flores <darthmincho@gmail.com>
2015-04-13 16:02:59 -0400

    Extended FT to support multiple lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
@@ -62,10 +63,10 @@ class NewVisitorTest(unittest.TestCase):
 
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+		self.fail('Finish the test!')
 
 		# She visits that URL - her to-do list is still there.
+		self.fail('Finish the test!')
 
 		# Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+		self.fail('Finish the test!')
